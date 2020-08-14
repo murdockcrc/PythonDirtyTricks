0fcac97c686780bf0c994a8e2e81b734a156f2b5
luisdel <luisdel@option40.com>
2020-08-14 17:52:15 +0200

    adds fast arrays

--- 01 - Collections/named_tuples.py
+++ 01 - Collections/named_tuples.py
@@ -2,15 +2,15 @@ import collections
 
 FootballTeam = collections.namedtuple('FootballTeam', ['name', 'country'])
 
+# Class definition with special methods implemented
 class Teams:
     countries = ['Germany', 'France', 'Spain']
     bundesliga_teams = ['RB Leipzig', 'Bayern Munich', 'Borussia Dortmund']
 
-    def __init__(self):
+    def __init__(self, country_name):
         # List comprehension with a filter on country name
-
         self._teams = [FootballTeam(name, country) for name in self.bundesliga_teams
-                                                   for country in self.countries if country == 'Germany']
+                                                   for country in self.countries if country == country_name]
 
     def __len__(self):
         return len(self._teams)
@@ -21,7 +21,7 @@ class Teams:
     def getAll(self):
         return self._teams
     
-teams = Teams()
+teams = Teams("Germany")
 
 print(len(teams))
 print(teams[0])

57f7ad9c00a045c1f9f18f89bed6e73be6c85b69
murdockcrc <murdockcrc@outlook.com>
2021-04-15 18:50:32 +0200

    Adds AppInsights logging sample

---
+++ azure_ml/logging/sample_step.py
@@ -0,0 +1,26 @@
+from azureml.core import Run
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+# Assumes the environment variable APPLICATIONINSIGHTS_CONNECTION_STRING is already set
+logger.addHandler(AzureLogHandler())
+logger.warning("I will be sent to Application Insights")
+
+# Add custom dimensions to trace
+run = Run.get_context(allow_offline=False)
+
+custom_dimensions = {
+    "parent_run_id": run.parent.id,
+    "step_id": run.id,
+    "step_name": run.name,
+    "experiment_name": run.experiment.name,
+    "run_url": run.parent.get_portal_url(),
+    "run_type": "training"
+}
+
+# Assumes AzureLogHandler was already registered above
+logger.info("I will be sent to Application Insights with Custom Dimensions", extra= {"custom_dimensions":custom_dimensions})
